34e8bf4a87b1d4195d91f0fd3eb48d33b9d69195
HaizaCasapao <casapaohaiza00@gmail.com>
2021-12-19 13:51:43 -1200

    init

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
+export default {
+    components: {
+      TheHeader,
+      TheResources  
+    }
+};
+</script>
+
+<style>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
